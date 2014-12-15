ea1cac0e5413afc0337b6c9a6d873225b54fc31b
Max Åberg <aaberg.max@gmail.com>
2014-12-16 00:02:00 +0100

    rate function added when options clicked

--- javascript/main.js
+++ javascript/main.js
@@ -4,29 +4,52 @@ $(document).ready(function(){
 			'max-height': '1.88em',
 			'max-width': '1.88em'
 		});
+
 		$('td', this).animate({
 			'font-size': '1.88em'
 		});
 	});
+
 	$('tr').mouseleave(function(){
 		$('img', this).animate({
 			'max-height': '1.25em',
 			'max-width': '1.25em'
 		});
+
 		$('td', this).animate({
 			'font-size': '1em'
 		});
 	});
+
 	$('#footer').children().mouseenter(function() {
 		$('img', this).animate({
 			'width': '5em',
 			'height': '5em'
 		});
 	});
+
 	$('#footer').children().mouseleave(function() {
 		$('img', this).animate({
 			'width': '4em',
 			'height': '4em'
 		});
 	});
+
+	$('tr').click(function() {
+		var rate;
+		$('#answer').empty(),
+		$('#questions').text('Please rate the overall service')
+		switch($(this).attr('id')){
+			case 'gen':
+				rate = 0.1;
+				break;
+			case 'rand':
+				rate = (Math.random() * 0.09 + 0.01).toFixed(2);
+				break;
+			case 'cheap':
+				rate = 0.01;
+				break;
+		}
+	});
+
 });
