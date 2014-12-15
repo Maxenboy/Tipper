7592f59a7b783543e6866b513e4b925cde5ed006
Max Åberg <aaberg.max@gmail.com>
2014-12-16 00:54:08 +0100

    Different star rating and stops at click

--- javascript/main.js
+++ javascript/main.js
@@ -41,17 +41,17 @@ $(document).ready(function(){
 		$('#questions').text('Please rate the overall service')
 		switch($(this).attr('id')){
 			case 'gen':
-				rate = 0.1;
-				break;
+			rate = 0.1;
+			break;
 			case 'rand':
-				rate = (Math.random() * 0.09 + 0.01).toFixed(2);
-				break;
+			rate = (Math.random() * 0.09 + 0.01).toFixed(2);
+			break;
 			case 'cheap':
-				rate = 0.01;
-				break;
+			rate = 0.01;
+			break;
 		}
 		$('#answer').addClass('rating'),
-		$('#answer').append('<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>')
+		$('#answer').append("<div class='stars'><div class='rating' ></div><input type='radio' id='star5'><label for='star5'></label><input type='radio' id='star4'><label for='star4'></label><input type='radio' id='star3'><label for='star3'></label><input type='radio' id='star2'><label for='star2'></label><input type='radio' id='star1'><label for='star1'></label></div>")
 	});
 
 });
