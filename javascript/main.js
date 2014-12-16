ac8ab4ee52cfb78fcca7ace5b4f8be461ec1766d
Max Åberg <aaberg.max@gmail.com>
2014-12-16 01:22:31 +0100

    fixed error with clikcing lower star rating

--- javascript/main.js
+++ javascript/main.js
@@ -50,8 +50,8 @@ $(document).ready(function(){
 			rate = 0.01;
 			break;
 		}
-		$('#answer').addClass('rating'),
-		$('#answer').append("<div class='stars'><div class='rating' ></div><input type='radio' id='star5'><label for='star5'></label><input type='radio' id='star4'><label for='star4'></label><input type='radio' id='star3'><label for='star3'></label><input type='radio' id='star2'><label for='star2'></label><input type='radio' id='star1'><label for='star1'></label></div>")
+		$('#answer').append("<div class='stars'><div class='rating'></div><input type='radio' name='rating' id='star5' value='5'><label for='star5'></label><input type='radio' name='rating' id='star4' value='4'><label for='star4'></label><input type='radio' name='rating' id='star3' value='3'><label for='star3'></label><input type='radio' name='rating' id='star2' value='2'><label for='star2'></label><input type='radio' name='rating' id='star1' value='1'><label for='star1'></label></div>")
 	});
 
 });
+
