93a41a89c09a14e77f29660f32defbee9ecbb689
Max Åberg <aaberg.max@gmail.com>
2014-12-17 20:42:21 +0100

    Detailed questions functionality done

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,6 @@
+var index = 0;
+var result = [];
+
 $(document).ready(function(){
 	$('tr').mouseenter(function(){
 		$('img', this).animate({
@@ -37,8 +40,8 @@ $(document).ready(function(){
 
 	$('tr').click(function() {
 		var character;
-		$('#answer').empty(),
-		$('#questions').text('Please rate the overall service')
+		emptyElement($('#answer'));
+		nextQuestion();
 		switch($(this).attr('id')){
 			case 'gen':
 			character = 0.1;
@@ -50,19 +53,62 @@ $(document).ready(function(){
 			character = 0.01;
 			break;
 		}
-		$('#answer').append("<div class='stars'><div class='rating'></div><input type='radio' name='rating' id='5' value='5'><label for='5'></label><input type='radio' name='rating' id='4' value='4'><label for='4'></label><input type='radio' name='rating' id='3' value='3'><label for='3'></label><input type='radio' name='rating' id='2' value='2'><label for='2'></label><input type='radio' name='rating' id='1' value='1'><label for='1'></label></div>")
 		localStorage.setItem('character',JSON.stringify(character));
 	});
 
 	$('body').on('click', 'label', function() {
-		var rating = parseInt($(this).attr('for'));
-		localStorage.setItem('rating', JSON.stringify(rating));
-		$('.detailed').empty(),
-		$('.detailed').append("<a class='leftoption' id='more'>Detailed service questions</a><a class='rightoption' id='Total amount'>To total amount</a>")
+		if (index == 1) {
+			var rating = parseInt($(this).attr('for'));
+			localStorage.setItem('rating', JSON.stringify(rating));
+			emptyElement($('.detailed'));
+			$('.detailed').append("<a class='leftoption' id='more'>Detailed service questions</a><a class='rightoption' id='Total amount'>To total amount</a>")
+		}else{
+			clickStar($(this));
+		}
+	});
 
+	$('body').on('click', '.leftoption', function() {
+		emptyElement($('.detailed'));
+		emptyElement($('#answer'));
+		nextQuestion();
+	});
+
+	$('body').on('click', '.rightoption', function() {
+		receipt();
+		
 	});
 
 });
 
+function nextQuestion(){
+	var questions = ['Please rate the overall service', 'How was the food?', 'How was the waiter/waitress?', 'How was the environment?']
+	var next = questions[index];
+	index ++;
+	if(index > questions.length){
+		receipt();
+	}else{
+		$('#questions').text(next);
+		createStars();
+	}
+}
 
+function createStars(){
+	$('#answer').append("<div class='stars'><div class='rating'></div><input type='radio' name='rating' id='5' value='5'><label for='5'></label><input type='radio' name='rating' id='4' value='4'><label for='4'></label><input type='radio' name='rating' id='3' value='3'><label for='3'></label><input type='radio' name='rating' id='2' value='2'><label for='2'></label><input type='radio' name='rating' id='1' value='1'><label for='1'></label></div>")
+}
+
+function emptyElement(element){
+	element.empty();
+}
+
+function clickStar(starElement){
+	result.push(starElement.attr('for'));
+	emptyElement($('#questions')),
+	emptyElement($('#answer'))
+	nextQuestion();
+}
+
+function receipt(){
+	emptyElement($('#questions')),
+	emptyElement($('#answer'))
+}
 
